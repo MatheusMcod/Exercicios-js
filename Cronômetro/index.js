d0d34154025f36fbe9212b6b5197f3a365b4e4b1
Matheus Moreira Costa <90786402+MatheusMcod@users.noreply.github.com>
2022-05-07 19:09:30 -0300

    Update

---
+++ Cronômetro/index.js
@@ -0,0 +1,22 @@
+"use strict"
+
+const iniciar = document.getElementById('iniciar')
+const pausar = document.getElementById('pausar')
+const reiniciar = document.getElementById('reiniciar')
+
+function contIniciar() {
+
+}
+
+function contPausar() {
+
+}
+
+function contReiniciar() {
+
+}
+
+
+iniciar.addEventListener('click', contIniciar)
+pausar.addEventListener('click', contPausar)
+reiniciar.addEventListener('click', contReiniciar)
